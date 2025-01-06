1f64f795904d4625ef82a218e4fe6ccf15cebad0
Shay Sarussi Elshten <shay@paraspot.co>
2025-01-06 14:48:02 +0200

    after slava review

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_pymongo import PyMongo
 from flask_cors import CORS
-from datetime import datetime
 from os import environ
 
 mongo = PyMongo()
+
+# Import user_bp outside the function
+from app.routes.user_routes import user_bp
 
 def create_app():
     app = Flask(__name__)
@@ -16,7 +18,7 @@ def create_app():
     
     mongo.init_app(app)
     
-    from app.routes.user_routes import user_bp
+    # Register the blueprint
     app.register_blueprint(user_bp)
     
     return app
